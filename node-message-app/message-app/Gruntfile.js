5000f258b84efdc7d8413a90d23ab7477128c791
pbriar <pbriar@live.com>
2018-07-12 11:01:18 -0400

    All dependencies installed

--- node-message-app/message-app/Gruntfile.js
+++ node-message-app/message-app/Gruntfile.js
@@ -5,9 +5,24 @@ module.exports = function (grunt) {
         files: {
           'public/stylesheets/style.css': 'sass/style.scss'
         }
+      },
+      options: {
+        includePaths: [
+          './bower_components/css-calc-mixin'
+        ]
+      }
+    },
+    watch: {
+      source: {
+        files: ['sass/**/*.scss', 'views/**/*.jade'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
       }
     }
   });
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-sass');
     grunt.registerTask('default', ['sass']);
 };
